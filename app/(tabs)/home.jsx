cb346d2d90f2fd9698ac569ef8cb8a9b7d249c68
Catalina <catalinacardenas459@gmail.com>
2024-09-17 22:47:27 -0500

    yapi el index y el sin in, pero el sign up tiene errores

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-100">
+      <Text className="text-2xl text-blue-500 font-bold">Home</Text>
+    </View>
+  );
+}
+
+export default Home;
